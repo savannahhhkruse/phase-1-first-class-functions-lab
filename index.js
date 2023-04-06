840ac7d8723523b6bae07d44685430436020cdfc
Savannah <skruse101@gmail.com>
2023-04-06 16:52:13 -0700

    first class functions advanced lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ["Autona", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = (drivers) =>{
+    return drivers.slice(0, 2);
+
+}
+
+
+const returnLastTwoDrivers = (drivers) =>{
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function (fare){
+    return function (multiplier){
+        return multiplier * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+    console.log(fareDoubler);
+
+const fareTripler = createFareMultiplier(3);
+    console.log(fareTripler);
+
+
+ const selectDifferentDrivers = function(drivers,array){
+    return array(drivers);
+ };  
